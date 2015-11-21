d983d9b943968d1fbd5b5dc5f054e8db51f8f9fb
Kamil Lelonek <squixy.sln@gmail.com>
2015-11-21 11:37:35 +0100

    Create tasks2.py

---
+++ tasks2.py
@@ -0,0 +1,11 @@
+upper_boundary = 101
+
+# TASK 2.1
+
+from_0_to_100 = [n for n in range(upper_boundary)]
+print(from_0_to_100)
+
+# TASK 2.2
+
+even_from_0_to_100 = [n for n in range(upper_boundary) if not n % 2]
+print(even_from_0_to_100)
