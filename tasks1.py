2b7101c6fd925a76f5da3bba9899e40b7ed0a06e
Kamil Lelonek <squixy.sln@gmail.com>
2015-11-21 11:39:25 +0100

    Update tasks1.py

--- tasks1.py
+++ tasks1.py
@@ -3,15 +3,12 @@ import string
 alphabet = string.ascii_lowercase
 
 # TASK 1.1
-
 for letter in alphabet: print(letter)
 
 # TASK 1.2
-
 print(alphabet[::2])
 
 # TASK 1.3
-
 def is_palindrome(word):
     initial_string  = word.replace(" ", "").lower()
     reversed_string = initial_string[::-1]
@@ -21,7 +18,6 @@ print(is_palindrome("abc"))
 print(is_palindrome("A to kanapa pana Kota"))
 
 # TASK 1.4
-
 def filter_non_ascii(word):
     return filter(lambda letter: letter in alphabet, word)
     
@@ -29,7 +25,6 @@ print(filter_non_ascii("xyz"))
 print(filter_non_ascii("ada1sd3a4"))
 
 # TASK 1.5
-
 def filter_longer_than_5(words):
     return filter(lambda word: len(word) > 5, words)
     
